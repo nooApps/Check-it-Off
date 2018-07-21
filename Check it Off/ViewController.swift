28c0a5685d6906d3ab3018ad72d2006e6cae258b
user141401 <user141401@tx014.local>
2018-07-21 10:02:29 -0700

    Initial Commit

---
+++ Check it Off/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Check it Off
+//
+//  Created by user141401 on 7/21/18.
+//  Copyright © 2018 NooApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
